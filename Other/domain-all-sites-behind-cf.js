4c873fc2080d625d24f7418690e3d93f11ad2fbe
coldwater-10 <91764021+coldwater-10@users.noreply.github.com>
2023-04-27 08:52:26 +0330

    Update domain-all-sites-behind-cf.js

--- Other/domain-all-sites-behind-cf.js
+++ Other/domain-all-sites-behind-cf.js
@@ -1,6 +1,6 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
-})
+});
 
 async function handleRequest(request) {
   const response = await fetch('https://raw.githubusercontent.com/hossein-mohseni/CF-Web/main/domains.json');
@@ -8,7 +8,9 @@ async function handleRequest(request) {
   let output = '';
   data.data.forEach(entry => {
     if (!entry.is_ir) {
-      output += `${entry.domain}\t\t\t in :  PLUS ENG\n`;
+      output += `${entry.domain}\t\t\t in : PLUS ENG\n`;
+    } else {
+      output += `${entry.domain}\t\t\t in : PLUS FA\n`;
     }
   });
   return new Response(output, {
