4a42582950d46444e238ac62ac756cc741d0e7b5
coldwater-10 <91764021+coldwater-10@users.noreply.github.com>
2023-05-27 03:31:43 +0330

    Update all.js

--- Clash/all.js
+++ Clash/all.js
@@ -271,6 +271,9 @@ cfw-proxies-order: latency
 external-controller: 0.0.0.0:9090
 secret: ''
 bind-address: '*'
+#hosts:
+  # '*.workers.dev': 185.59.218.86
+  # '.workers.dev': 185.59.218.86
 unified-delay: true
 profile:
   store-selected: true
